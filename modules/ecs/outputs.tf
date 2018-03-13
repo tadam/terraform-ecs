4014d543a572a3a26962260fe33cc01f1d84cde2
Yury Zavarin <yury.zavarin@gmail.com>
2018-03-13 22:44:57 +0300

    Added ECS service definition for tcbuildreport-backend.

--- modules/ecs/outputs.tf
+++ modules/ecs/outputs.tf
@@ -1,3 +1,11 @@
 output "default_alb_target_group" {
   value = "${module.alb.default_alb_target_group}"
 }
+
+output "ecs_cluster_arn" {
+  value = "${aws_ecs_cluster.cluster.arn}"
+}
+
+output "ecs_lb_role_arn" {
+  value = "${aws_iam_role.ecs_lb_role.arn}"
+}
--- tcbuildreport.tf
+++ tcbuildreport.tf
@@ -2,3 +2,17 @@ resource "aws_ecs_task_definition" "tcbuildreport-backend" {
   family = "tcbuildreport-backend"
   container_definitions = "${file("td-tcbuildreport-backend.json")}"
 }
+
+resource "aws_ecs_service" "tcbuildreport-backend" {
+  name = "tcbuildreport-backend"
+  cluster = "${module.ecs.ecs_cluster_arn}"
+  task_definition = "${aws_ecs_task_definition.tcbuildreport-backend.arn}"
+  desired_count   = 2
+  iam_role = "${module.ecs.ecs_lb_role_arn}"
+
+  load_balancer {
+    target_group_arn = "${module.ecs.default_alb_target_group}"
+    container_name = "tcbuildreport-backend"
+    container_port = 8080
+  }
+}
